ea2e879a353c43cccbe87976e13b726eabd0a917
Matewanga <102257709+Matewanga@users.noreply.github.com>
2025-10-30 01:49:39 -0300

    Desafio 01

--- Instituto Joule/Lab_AI/Desafio 01 - Analise de Imagem/components/Icons.tsx
+++ Instituto Joule/Lab_AI/Desafio 01 - Analise de Imagem/components/Icons.tsx
@@ -1,4 +1,5 @@
 
+
 import React from 'react';
 
 interface IconProps {
@@ -40,3 +41,17 @@ export const LinkedinIcon: React.FC<IconProps> = ({ className }) => (
     <path d="M20.5 2h-17A1.5 1.5 0 0 0 2 3.5v17A1.5 1.5 0 0 0 3.5 22h17a1.5 1.5 0 0 0 1.5-1.5v-17A1.5 1.5 0 0 0 20.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 1 1 8.25 6.5 1.75 1.75 0 0 1 6.5 8.25zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0 0 13 14.19a.66.66 0 0 0 0 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 0 1 2.7-1.4c1.55 0 3.36.86 3.36 3.66z"/>
   </svg>
 );
+
+export const ArrowUpIcon: React.FC<IconProps> = ({ className }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={className}
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75 12 8.25l7.5 7.5" />
+  </svg>
+);
+
